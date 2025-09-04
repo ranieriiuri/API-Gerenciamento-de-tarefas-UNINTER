848414d5b057b873667fec655653e4ac643cd64c
ranieriiuri <iuriranierioliveira@gmail.com>
2025-09-04 18:30:38 -0300

    Inicializando apartir do 'generate' do initializer

---
+++ src/main/java/com/ranieriiuri/gerenciadorDeTarefasAPI/GerenciadorDeTarefasApiApplication.java
@@ -0,0 +1,13 @@
+package com.ranieriiuri.gerenciadorDeTarefasAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeTarefasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeTarefasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranieriiuri/gerenciadorDeTarefasAPI/GerenciadorDeTarefasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranieriiuri.gerenciadorDeTarefasAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTarefasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
